0d6559ce626be5f897ffd9f246be67347af5699e
Mary Martinez <mlmartinez85@gmail.com>
2016-11-22 12:19:49 -0800

    Milestone 4

--- Tinder/CardsViewController.swift
+++ Tinder/CardsViewController.swift
@@ -23,6 +23,9 @@ class CardsViewController: UIViewController {
     @IBAction func didPan(_ sender: UIPanGestureRecognizer) {
         
         let translation = sender.translation(in: profileImageView)
+        let velocity = sender.velocity(in: self.view)
+        
+        var toValue: Double = 0.0
         
         if sender.state == .began {
             imageOriginalCener = profileImageView.center
@@ -30,6 +33,20 @@ class CardsViewController: UIViewController {
             
         } else if sender.state == .changed {
             print("changed")
+            
+            if velocity.x > 0 { // rotate clockwise
+                toValue = -(M_PI)
+                
+            } else { // rotate counter clockwise
+                toValue = M_PI
+            }
+            
+            let rotationAnimation = CABasicAnimation(keyPath: "transform.rotation")
+            rotationAnimation.fromValue = 0.0
+            rotationAnimation.toValue = toValue
+            rotationAnimation.duration = 3.0
+            
+            self.profileImageView.layer.add(rotationAnimation, forKey: nil)
             
             UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseOut, animations: {
                 self.profileImageView.center.x = translation.x
